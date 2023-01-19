69748c3bb4f2fe37534af965e90b6a4e12e07f60
Mario Saputra <m.saputra@future-s.com>
2023-01-19 02:50:27 +0000

    access store state

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,25 @@
       <h1>Pinia Tasks</h1>
     </header>
 
+    <!-- task list -->
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks">
+        <TaskDetails :task="task"/>
+      </div>
+    </div>
+
   </main>
 </template>
 
 <script>
+  import TaskDetails from './components/TaskDetails.vue'
+  import { useTaskStore } from './stores/TaskStore';
+
   export default {
-    
+    components:{ TaskDetails }, 
+    setup() {
+      const taskStore = useTaskStore()
+      return { taskStore }
+    }
   }
 </script>
