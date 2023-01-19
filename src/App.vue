9c2aaebe7af7daca421492c2d56aaa5edebcb9ee
Mario Saputra <m.saputra@future-s.com>
2023-01-19 08:58:43 +0000

    toggle display all/favs

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,22 @@
       <h1>Pinia Tasks</h1>
     </header>
 
+    <button @click="filter = 'all'">
+      Display All
+    </button>
+    <button @click="filter = 'favs'">
+      Display Favs
+    </button>
+
     <!-- task list -->
-    <div class="task-list">
+    <div class="task-list" v-if="filter === 'all'">
       <p>All tasks</p>
       <div v-for="task in taskStore.tasks">
         <TaskDetails :task="task"/>
       </div>
     </div>
 
-    <div class="task-list">
+    <div class="task-list" v-if="filter === 'favs'">
       <p>Favourite Tasks</p>
       <div v-for="task in taskStore.favs">
         <TaskDetails :task="task"/>
@@ -28,12 +35,16 @@
 <script>
   import TaskDetails from './components/TaskDetails.vue'
   import { useTaskStore } from './stores/TaskStore';
+  import { ref } from 'vue';
 
   export default {
     components:{ TaskDetails }, 
     setup() {
       const taskStore = useTaskStore()
-      return { taskStore }
+
+      const filter = ref('all')
+
+      return { taskStore, filter }
     }
   }
 </script>
